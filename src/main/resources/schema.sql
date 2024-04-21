27dddbe41012bad3b542aad47f56b0aaa860ed31
Wen Xie <wenxie65@gmail.com>
2024-04-21 17:03:01 -0400

    Update data structure

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,12 +15,11 @@ CREATE TABLE state (
 -- Table structure for town
 DROP TABLE IF EXISTS town;
 CREATE TABLE town (
-	townId INT AUTO_INCREMENT,
+    zipcode VARCHAR(10) NOT NULL,
     townName VARCHAR(100) NOT NULL,
     stateAbbrev VARCHAR(2)  NOT NULL,
-    zipcode VARCHAR(10) NOT NULL,
     CONSTRAINT pk_town
-		PRIMARY KEY (townId),
+		PRIMARY KEY (zipcode),
 	CONSTRAINT fk_town_state
 		FOREIGN KEY (stateAbbrev)
 			REFERENCES state(stateAbbrev)
@@ -33,26 +32,26 @@ DROP TABLE IF EXISTS street;
 CREATE TABLE street (
 	streetId INT AUTO_INCREMENT,
     streetName VARCHAR(100) NOT NULL,
-    townId INT NOT NULL,
+    zipcode VARCHAR(10) NOT NULL,
     CONSTRAINT pk_street
 		PRIMARY KEY (streetId),
 	CONSTRAINT fk_street_town
-		FOREIGN KEY (townId)
-			REFERENCES town(townId)
+		FOREIGN KEY (zipcode)
+			REFERENCES town(zipcode)
             ON UPDATE CASCADE
 			ON DELETE CASCADE
 );
 
--- Table structure for address
-DROP TABLE IF EXISTS address;
-CREATE TABLE address (
-	addressId INT AUTO_INCREMENT,
+-- Table structure for house
+DROP TABLE IF EXISTS house;
+CREATE TABLE house (
+	houseId INT AUTO_INCREMENT,
     houseNumber INT NOT NULL,
     aptNumber VARCHAR(20),
     streetId INT NOT NULL,
-    CONSTRAINT pk_address
-		PRIMARY KEY (addressId),
-	CONSTRAINT fk_address_street
+    CONSTRAINT pk_house
+		PRIMARY KEY (houseId),
+	CONSTRAINT fk_house_street
 		FOREIGN KEY (streetId)
 			REFERENCES street(streetId)
 			ON UPDATE CASCADE
@@ -69,21 +68,21 @@ CREATE TABLE customer (
 		PRIMARY KEY (phoneNumber)
 );
 
--- Table structure for customer_address bridge table
-DROP TABLE IF EXISTS customer_address;
-CREATE TABLE customer_address (
+-- Table structure for customer_house bridge table
+DROP TABLE IF EXISTS customer_house;
+CREATE TABLE customer_house (
 	phoneNumber VARCHAR(15) NOT NULL,
-    addressId INT NOT NULL,
-    CONSTRAINT pk_customer_address
-		PRIMARY KEY (phoneNumber, addressId),
-	CONSTRAINT fk_customer_address_customer
+    houseId INT NOT NULL,
+    CONSTRAINT pk_customer_house
+		PRIMARY KEY (phoneNumber, houseId),
+	CONSTRAINT fk_customer_house_customer
 		FOREIGN KEY (phoneNumber)
 			REFERENCES customer(phoneNumber)
             ON UPDATE CASCADE
 			ON DELETE CASCADE,
-	CONSTRAINT fk_customer_address_address
-		FOREIGN KEY (addressId)
-			REFERENCES address(addressId)
+	CONSTRAINT fk_customer_house_address
+		FOREIGN KEY (houseId)
+			REFERENCES house(houseId)
             ON UPDATE CASCADE
 			ON DELETE CASCADE
 );
